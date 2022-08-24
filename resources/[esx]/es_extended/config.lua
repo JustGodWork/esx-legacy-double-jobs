e8cf4e1f8032df317a3cb13d7943eb04529fc7d1
JustGod <discord1elie@gmail.com>
2022-08-24 05:01:31 +0200

    first commit

---
+++ resources/[esx]/es_extended/config.lua
@@ -0,0 +1,67 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.Accounts = {
+	bank = _U('account_bank'),
+	black_money = _U('account_black_money'),
+	money = _U('account_money')
+}
+
+Config.identifier 				= "steam"
+Config.StartingAccountMoney 	= {bank = 50000}
+
+Config.EnableSocietyPayouts 	= false -- pay from the society account that the player is employed at? Requirement: esx_society
+Config.EnableHud            	= true -- enable the default hud? Display current job and accounts (black, bank & cash)
+Config.MaxWeight            	= 24   -- the max inventory weight without backpack
+Config.PaycheckInterval         = 7 * 60000 -- how often to recieve pay checks in milliseconds
+Config.EnableDebug              = false -- Use Debug options?
+Config.EnableDefaultInventory   = true -- Display the default Inventory ( F2 )
+Config.EnableWantedLevel    	= false -- Use Normal GTA wanted Level?
+Config.EnablePVP                = true -- Allow Player to player combat
+Config.NativeNotify             = false -- use old native notification?
+
+Config.Multichar                = false -- Enable support for esx_multicharacter
+Config.Identity                 = true -- Select a characters identity data before they have loaded in (this happens by default with multichar)
+Config.DistanceGive 			= 4.0 -- Max distance when giving items, weapons etc.
+Config.OnDuty                   = true -- Default state of the on duty system
+Config.DisableHealthRegen         = false -- Player will no longer regenerate health
+Config.DisableVehicleRewards      = true -- Disables Player Recieving weapons from vehicles
+Config.DisableNPCDrops            = true -- stops NPCs from dropping weapons on death
+Config.DisableWeaponWheel         = false -- Disables default weapon wheel
+Config.DisableAimAssist           = true -- disables AIM assist (mainly on controllers)
+Config.RemoveHudCommonents = {
+	[1] = true, --WANTED_STARS,
+	[2] = false, --WEAPON_ICON
+	[3] = true, --CASH
+	[4] = true,  --MP_CASH
+	[5] = false, --MP_MESSAGE
+	[6] = false, --VEHICLE_NAME
+	[7] = false,-- AREA_NAME
+	[8] = true,-- VEHICLE_CLASS
+	[9] = false, --STREET_NAME
+	[10] = false, --HELP_TEXT
+	[11] = false, --FLOATING_HELP_TEXT_1
+	[12] = false, --FLOATING_HELP_TEXT_2
+	[13] = true, --CASH_CHANGE
+	[14] = false, --RETICLE
+	[15] = false, --SUBTITLE_TEXT
+	[16] = false, --RADIO_STATIONS
+	[17] = true, --SAVING_GAME,
+	[18] = false, --GAME_STREAM
+	[19] = false, --WEAPON_WHEEL
+	[20] = false, --WEAPON_WHEEL_STATS
+	[21] = false, --HUD_COMPONENTS
+	[22] = false, --HUD_WEAPONS
+}
+
+Config.CustomAIPlates = 'ESX.A111' -- Custom plates for AI vehicles 
+-- Pattern string format
+--1 will lead to a random number from 0-9.
+--A will lead to a random letter from A-Z.
+-- . will lead to a random letter or number, with 50% probability of being either.
+--^1 will lead to a literal 1 being emitted.
+--^A will lead to a literal A being emitted.
+--Any other character will lead to said character being emitted.
+-- A string shorter than 8 characters will be padded on the right.
+
+Config.MaxAdminVehicles = true -- admin vehicles spawn with max vehcle settings
---
+++ resources/[esx_addons]/esx_weaponshop/locales/it.lua
@@ -0,0 +1,13 @@
+Locales ['it'] = {
+    ['buy_license'] = 'acquistare porto il d\'armi?',
+    ['yes'] = '%si',
+    ['no'] = 'no',
+    ['weapon_bought'] = 'acquistato per $%s',
+    ['not_enough_black'] = 'non hai ~r~abbastanza soldi sporchi',
+    ['not_enough'] = 'non hai ~r~abbastanza soldi',
+    ['already_owned'] = 'Hai già quest arma!',
+    ['shop_menu_title'] = 'Ammu-Nation',
+    ['shop_menu_prompt'] = 'premere [E] per accedere al negozio',
+    ['shop_menu_item'] = '$%s',
+    ['map_blip'] = 'Ammu-Nation',
+  }
